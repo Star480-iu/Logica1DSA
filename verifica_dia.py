de5a54e0fa296b3c6e43e156d0597f3b04082c63
Joan <joana.sofia.ramos@Escola.pr.gov.br>
2025-12-01 11:35:14 -0300

    Create verifica_dia.py

---
+++ verifica_dia.py
@@ -0,0 +1,10 @@
+# Programa Verificador do Dia da Semana
+
+# Criando a variável para armazenar o nome do dia
+dia = "Sábado"  # você pode alterar para testar outros dias
+
+# Estrutura condicional para verificar se é final de semana
+if dia == "Sábado" or dia == "Domingo":
+    print("É final de semana!")
+else:
+    print("É dia de semana.")
