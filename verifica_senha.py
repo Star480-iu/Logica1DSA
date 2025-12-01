5bde2f15844e6b22cff9713876c6d91ff37597fe
Joan <joana.sofia.ramos@Escola.pr.gov.br>
2025-12-01 11:35:53 -0300

    Create verifica_senha.py

---
+++ verifica_senha.py
@@ -0,0 +1,13 @@
+# Programa Verificador de Senha Simples
+
+# Variável que armazena a senha correta
+senha_correta = "12345"
+
+# Variável que armazena a senha digitada pelo usuário
+senha_digitada = "12345"  # você pode alterar para testar
+
+# Estrutura condicional SE-SENÃO
+if senha_digitada == senha_correta:
+    print("Login bem-sucedido!")
+else:
+    print("Senha incorreta. Tente novamente.")
