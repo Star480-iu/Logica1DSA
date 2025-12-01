4df58ee6f4c7770d5a0108fe97749fc64e77c428
Joan <joana.sofia.ramos@Escola.pr.gov.br>
2025-12-01 11:34:10 -0300

    Create verifica_idade.py

---
+++ verifica_idade.py
@@ -0,0 +1,10 @@
+# Programa Verificador de Idade
+
+# Criando a variável para armazenar a idade
+idade = 17  # você pode alterar o valor para testar
+
+# Estrutura condicional SE-SENÃO
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
