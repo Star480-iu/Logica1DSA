415556a20b2ad585c4dbfe83750cfac145075002
Joan <joana.sofia.ramos@Escola.pr.gov.br>
2025-12-01 11:36:37 -0300

    Create classifica_numero.py

---
+++ classifica_numero.py
@@ -0,0 +1,12 @@
+# Programa Classificador de Número
+
+# Variável para armazenar o número
+numero = -5  # você pode alterar para testar
+
+# Estruturas condicionais
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else:
+    print("O número é zero.")
